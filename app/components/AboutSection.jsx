9ff0241637f63912f23dac448cc59664f5e5ef48
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-11-18 18:23:36 +0300

    ✅ Add About Me Section

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,108 @@
+"use client";
+import React, { useState, useTransition } from "react";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Next.js</li>
+        <li>HTML</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Master of Science in Computer Science</li>
+        <li>Bachelor of Science in Computer Science</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experience",
+    id: "experience",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Software Engineer at Google</li>
+        <li>Software Engineer at Facebook</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Google Cloud Professional Cloud Architect</li>
+        <li>Google Cloud Professional Data Engineer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("Skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white ">
+      <div className="flex items-center gap-8 py-8 px-4 xl:gap-16 sm:px-16 xl:px-16">
+        <div className="">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            mollitia blanditiis cupiditate asperiores autem, animi nesciunt
+            praesentium? Ex assumenda nihil placeat, fugiat incidunt animi
+            libero delectus consequuntur quaerat nostrum, sit natus, possimus
+            quidem vitae itaque in. Natus tenetur expedita pariatur?
+          </p>
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Educations
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
+            >
+              Experience
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab)?.content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
