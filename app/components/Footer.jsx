e73df0df2141ba24f3a4d0956768081dd4022bc0
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-11-20 00:24:10 +0300

    🛠️ Add Footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
+      <div className="container p-12 m-2 flex justify-center">
+        <p className="text-white">Made with ❤️ by Mamdouh</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
