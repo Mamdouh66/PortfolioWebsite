5d938cf37b9cf70b5bea70d2928710ee88b95272
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-11-20 15:18:44 +0300

    🛠️ Fix issue with ESlint rules

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -43,7 +43,9 @@ const EmailSection = () => {
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-red-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">
+          Let &apos;s Connect
+        </h5>
         <p className="text-[#dfdfdf] mb-4 max-w-md">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
           temporibus aspernatur ea maiores iusto obcaecati molestiae nihil magni
