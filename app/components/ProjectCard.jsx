3e4e33a754a5115e25e9794ebfac64fa647ecafe
Mamdouh Khalaf <2210002016@iau.edu.sa>
2024-02-11 11:00:51 +0300

    🔧 UPDATE: ProjectCard component and add new project images

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import Image from "next/image";
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
+const ProjectCard = ({ image, title, description, gitUrl, previewUrl }) => {
   return (
     <div className="font-code">
-      <div
-        className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      >
-        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-          <Link
-            href={gitUrl}
-            className="h-14 w-14 border-2 mr-2 relative rounded-full border-[#dfdfdf] hover:border-white group/link"
-          >
-            <CodeBracketIcon className="h-10 w-10 text-[#dfdfdf]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-gray-500" />
-          </Link>
+      <div className="h-52 md:h-72 rounded-t-xl relative group">
+        <div className="absolute inset-0">
+          <Image
+            src={image}
+            alt={title}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-t-xl blur-sm"
+          />
+        </div>
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-50 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+          {gitUrl ? (
+            <Link
+              href={gitUrl}
+              className="h-14 w-14 border-2 mr-2 relative rounded-full border-[#dfdfdf] hover:border-white group/link"
+            >
+              <CodeBracketIcon className="h-10 w-10 text-[#dfdfdf]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-gray-500" />
+            </Link>
+          ) : (
+            <div
+              className="h-14 w-14 border-2 mr-2 relative rounded-full border-[#dfdfdf] hover:border-white group/link"
+              title="There is no git URL for this project"
+            >
+              <CodeBracketIcon className="h-10 w-10 text-[#dfdfdf]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-gray-500" />
+            </div>
+          )}
           <Link
             href={previewUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#dfdfdf] hover:border-white group/link"
--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -5,48 +5,50 @@ import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
 
+import SanadLogo from "../../public/logos/Sanad-website.png";
+import FastHacksImage from "../../public/logos/fasthacks.png";
+
 const projectsData = [
   {
     id: 1,
-    title: "Project 1",
+    title: "Sanad's Website",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    image: "https://random.imagecdn.app/500/120",
-    tags: ["All", "Web", "Ai"],
-    gitUrl: "https://github.com",
+    image: SanadLogo,
+    tags: ["All", "Web"],
+    gitUrl: "",
     previewUrl: "https://github.com",
   },
-  // generate three more projectsData objects here
   {
     id: 2,
-    title: "Project 2",
+    title: "FastHacks Website",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    image: "https://random.imagecdn.app/500/130",
+      "I made this website for a hackathon called FastHacks that we organized in our university.",
+    image: FastHacksImage,
     tags: ["All", "Web"],
     gitUrl: "https://github.com",
     previewUrl: "https://github.com",
   },
-  {
-    id: 3,
-    title: "Project 3",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    image: "https://random.imagecdn.app/500/140",
-    tags: ["All", "Ai"],
-    gitUrl: "https://github.com",
-    previewUrl: "https://github.com",
-  },
-  {
-    id: 4,
-    title: "Project 4",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    image: "https://random.imagecdn.app/500/150",
-    tags: ["All", "Ai"],
-    gitUrl: "https://github.com",
-    previewUrl: "https://github.com",
-  },
+  // {
+  //   id: 3,
+  //   title: "Project 3",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //   image: "https://random.imagecdn.app/500/140",
+  //   tags: ["All", "Ai"],
+  //   gitUrl: "https://github.com",
+  //   previewUrl: "https://github.com",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Project 4",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //   image: "https://random.imagecdn.app/500/150",
+  //   tags: ["All", "Ai"],
+  //   gitUrl: "https://github.com",
+  //   previewUrl: "https://github.com",
+  // },
 ];
 
 const ProjectSection = () => {
@@ -104,7 +106,7 @@ const ProjectSection = () => {
               key={project.id}
               title={project.title}
               description={project.description}
-              imgUrl={project.image}
+              image={project.image}
               tags={project}
               gitUrl={project.gitUrl}
               previewUrl={project.previewUrl}
